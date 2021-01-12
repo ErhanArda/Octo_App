d8528b56b8a2baf513c5ab269f29acfeb0f5018d
ErhanArda <erhanarda7@gmail.com>
2021-01-12 17:17:34 +0300

    search added

--- src/components/UserListItem.vue
+++ src/components/UserListItem.vue
@@ -15,10 +15,11 @@
             <th class="text-left">
                 Email
             </th>
+                <th><v-text-field v-model="searchText" label="Search" required></v-text-field></th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="user in userList" :key="user.id">
+            <tr v-for="user in filteredUserList" :key="user.id">
             <td>{{ user.id }}</td>
             <td>{{ user.first_name }}</td>
             <td>{{ user.last_name }}</td>
@@ -45,6 +46,11 @@ import UpdateUserInfo from "./UpdateUserInfo";
 export default {
     components:{UpdateUserInfo},
     props: ["userList"],
+    data() {
+        return {
+        searchText: "",
+        };
+    },
     methods:{
         deleteUser(userId){
         const willDelete = confirm(`Do you really want to delete this record? ${userId}`)
@@ -52,6 +58,15 @@ export default {
         this.$store.dispatch("deleteUser",userId)
         }
         },
+    },
+    computed:{
+        filteredUserList(){
+        return this.userList.filter((user) =>
+            user.first_name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+            user.last_name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+            user.email.includes(this.searchText)
+        );
+        }
     }
 };
 </script>
